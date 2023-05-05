89c94420b304a9f4de0d1260953906bbb79f5ec4
Shifat ullah <shifatullah128@gmail.com>
2023-05-05 19:16:04 +0600

    quizContainer error problem fixed

--- index.js
+++ index.js
@@ -46,25 +46,27 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
+ 
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
-
+ 
   data.forEach((quiz, i) => {
+  
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -72,6 +74,7 @@ const displayQuiz = (data) => {
 </div>`;
   });
 };
+
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
